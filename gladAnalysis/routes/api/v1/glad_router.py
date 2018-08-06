ecad8084b51f81dcfd65e3cb18eaa9f6912fd08c
mappingvermont <charlie.hofmann@gmail.com>
2018-08-06 17:54:22 -0400

    add /download endpoint for testing purposes

--- gladAnalysis/routes/api/v1/glad_router.py
+++ gladAnalysis/routes/api/v1/glad_router.py
@@ -51,17 +51,24 @@ def glad_download_geom_input():
 
 
 # send ISO, download from s3
-@glad_analysis_endpoints.route('/download/<iso_code>/', methods=['GET'])
-@glad_analysis_endpoints.route('/download/<iso_code>/<adm1_code>/', methods=['GET'])
-@glad_analysis_endpoints.route('/download/<iso_code>/<adm1_code>/<adm2_code>/', methods=['GET'])
-def glad_download_iso_input(iso_code, adm1_code=None, adm2_code=None):
+@glad_analysis_endpoints.route('/download', methods=['GET'])
+def test_bra1():
 
-    streaming = analysis_services.iso_download(request, iso_code, adm1_code, adm2_code)
+    # https://stackoverflow.com/a/16890018
+    s3_conn = S3Connection() 
+    bucket_obj = s3_conn.get_bucket('gfw2-data')
 
     def generate():
-        for row in streaming:
-            if row:
-                yield row + '\n'
+        for f in bucket_obj.list(prefix='alerts-tsv/temp/glad-by-state/BRA/BRA_1.csv'):
+            unfinished_line = ''
+            for byte in f:
+                byte = unfinished_line + byte
+                lines = byte.split('\n')
+                unfinished_line = lines.pop()
+                for line in lines:
+                    yield line + '\n'
+
+            yield unfinished_line
 
     return Response(generate(), mimetype='text/csv')
 
