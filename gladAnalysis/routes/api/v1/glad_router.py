b6604121588ca95a83484bb458237b61fb341405
mappingvermont <charlie.hofmann@gmail.com>
2018-09-06 16:40:03 -0400

    remove other references to streaming download, add geostore simplify

--- gladAnalysis/routes/api/v1/glad_router.py
+++ gladAnalysis/routes/api/v1/glad_router.py
@@ -4,7 +4,6 @@ from boto.s3.connection import S3Connection
 
 from gladAnalysis.utils import util
 from gladAnalysis.serializers import serialize_response
-from gladAnalysis.services import download_service
 
 glad_analysis_endpoints = Blueprint('glad_analysis_endpoints', __name__)
 
@@ -19,7 +18,11 @@ def glad_stats_iso(iso_code, adm1_code=None, adm2_code=None):
     route = util.route_constructor(iso_code, adm1_code, adm2_code)
 
     alerts_uri = '/glad-alerts/summary-stats/admin/{}?{}'.format(route, query_params)
-    area_uri = '/geostore/admin/{}'.format(route)
+    area_uri = '/v2/geostore/admin/{}'.format(route)
+
+    # if it's just an iso, simplify the geostore because we only really want the area_ha
+    if not adm1_code:
+        area_uri += '?simplify=0.05'
 
     glad_alerts = util.query_microservice(alerts_uri)
     glad_area = util.query_microservice(area_uri)['data']['attributes']['areaHa']
