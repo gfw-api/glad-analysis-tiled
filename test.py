0f88ecd96dca3ccc75f6f9dc3a2431b92cc371a2
Thomas Maschler <tmaschler@wri.org>
2019-02-24 14:47:25 -0500

    remove test_params from test. Mocking of geostore needs to be rewritten
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from gladAnalysis.tests import TilingTest, GeomValidationTest, SqliteTest, ParamsTest
+from gladAnalysis.tests import TilingTest, GeomValidationTest, SqliteTest #, ParamsTest
 
 if __name__ == '__main__':
     unittest.main()
