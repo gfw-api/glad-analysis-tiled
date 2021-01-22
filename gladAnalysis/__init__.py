c12e7a0e299b67f138e9388406e84c86b395117e
Tiago Garcia <tiagojsag@gmail.com>
2021-01-22 15:19:53 +0100

    Replace CT integration library

--- gladAnalysis/__init__.py
+++ gladAnalysis/__init__.py
@@ -3,7 +3,7 @@
 import logging
 import os
 
-import CTRegisterMicroserviceFlask
+import RWAPIMicroservicePython
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
@@ -35,15 +35,17 @@ logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
 # CT
 info = load_config_json('register')
 swagger = load_config_json('swagger')
-CTRegisterMicroserviceFlask.register(
+RWAPIMicroservicePython.register(
     app=app,
     name='gladAnalysis',
     info=info,
     swagger=swagger,
-    mode=CTRegisterMicroserviceFlask.AUTOREGISTER_MODE if os.getenv('CT_REGISTER_MODE') and os.getenv(
-        'CT_REGISTER_MODE') == 'auto' else CTRegisterMicroserviceFlask.NORMAL_MODE,
+    mode=RWAPIMicroservicePython.AUTOREGISTER_MODE if os.getenv('CT_REGISTER_MODE') and os.getenv(
+        'CT_REGISTER_MODE') == 'auto' else RWAPIMicroservicePython.NORMAL_MODE,
     ct_url=os.getenv('CT_URL'),
-    url=os.getenv('LOCAL_URL')
+    url=os.getenv('LOCAL_URL'),
+    token=os.getenv('CT_TOKEN'),
+    api_version=os.getenv('API_VERSION')
 )
 
 
